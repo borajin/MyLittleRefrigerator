a9196d5ac459d6d6f10d3221cbc59a711d7e5835
BORA JIN <borajin.soft@gmail.com>
2020-09-17 14:05:19 +0900

    upload github

---
+++ java/OnDatabaseCallback.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+import java.util.ArrayList;
+
+public interface OnDatabaseCallback {
+    public void insert(String name, String date);
+    public void update(String oldName, String name, String date);
+    public void delete(String name);
+    public String search(String name);
+    public ArrayList<StuffInfo> selectAll();
+}
+
